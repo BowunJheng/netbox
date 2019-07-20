8fd40983706730a1a47d31a870f3b045aace5fa4
Bowun Jheng <bowen@chu.edu.tw>
2019-07-20 12:31:23 +0800

    update..

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-support_service=( pihole/pihole:latest adguard/adguardhome:arm64-edge )
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx.key -out nginx.crt
+support_service=( pihole/pihole:latest adguard/adguardhome:arm64-edge nginx:latest php:rc-fpm postgres:latest )
 support_func="start|stop|restart|update|status"
 service_array=()
 
@@ -7,7 +8,12 @@ list() {
 	counter=1
 	for name in "${support_service[@]}"
 	do
-		echo "./${0} ${support_func} ${counter}|${name%%'/'*}|${name}"
+		if [[ $name == *"/"* ]];
+		then
+			echo "./${0} ${support_func} ${counter}|${name%%'/'*}|${name}"
+		else
+			echo "./${0} ${support_func} ${counter}|${name%%':'*}|${name}"
+		fi
 		((counter++))
 	done
 	echo "./${0} ${support_func} all"
@@ -22,7 +28,7 @@ checkargv() {
 		counter=1
 		for name in "${support_service[@]}"
 		do
-			if [[ ("${1}" == "${counter}") || ("${1%%'/'*}" == "${name%%'/'*}") || (${1} == ${name}) ]];
+			if [[ ("${1}" == "${counter}") || ("${1%%'/'*}" == "${name%%'/'*}") || ("${1%%':'*}" == "${name%%':'*}") || (${1} == ${name}) ]];
 			then
 				service_array=( ${name} )
 				break
@@ -41,142 +47,60 @@ start() {
 	echo "start [${1}]"
 	docker pull ${1}
 	lanip=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
+	dockerstr=""
 	case ${1} in
 		"pihole/pihole:latest")
-			docker run -d \
-				--name pihole \
-				--sysctl net.ipv6.conf.all.disable_ipv6=1 \
-				-p ${lanip}:853:53/tcp -p ${lanip}:853:53/udp \
-				-e TZ="Asia/Taipei" \
-				-e IPv6=False \
-				-e DNS1="208.67.222.222" \
-				-e DNS2="208.67.220.220" \
-				-v "$(pwd)/etc-pihole/:/etc/pihole/" \
-				-v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-				--restart=unless-stopped \
-				${1}
+			dockerstr+="--name pihole "
+			dockerstr+="-p ${lanip}:853:53/tcp -p ${lanip}:853:53/udp "
+			dockerstr+="-e TZ=Asia/Taipei "
+			dockerstr+="-e IPv6=False "
+			dockerstr+="-e DNS1=208.67.222.222 "
+			dockerstr+="-e DNS2=208.67.220.220 "
+			dockerstr+="-v $(pwd)/etc-pihole/:/etc/pihole/ "
+			dockerstr+="-v $(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/ "
+			dockerstr+="--restart=unless-stopped "
 		;;
 		"adguard/adguardhome:arm64-edge")
 			mkdir $(pwd)/etc-adguardconf/
-			echo 'bind_host: 0.0.0.0
-bind_port: 3000
-auth_name: alarm
-auth_pass: 3146alarm
-language: ""
-rlimit_nofile: 0
-dns:
-  bind_host: 0.0.0.0
-  port: 53
-  protection_enabled: true
-  filtering_enabled: true
-  blocking_mode: mxdomain
-  blocked_response_ttl: 10
-  querylog_enabled: true
-  ratelimit: 20
-  ratelimit_whitelist: []
-  refuse_any: true
-  bootstrap_dns:
-  - 1.1.1.1:53
-  all_servers: false
-  allowed_clients: []
-  disallowed_clients: []
-  blocked_hosts: []
-  parental_sensitivity: 13
-  parental_enabled: true
-  safesearch_enabled: true
-  safebrowsing_enabled: true
-  resolveraddress: ""
-  upstream_dns:
-  - 10.10.10.10:853
-tls:
-  enabled: false
-  server_name: ""
-  force_https: false
-  port_https: 443
-  port_dns_over_tls: 853
-  certificate_chain: ""
-  private_key: ""
-filters:
-- enabled: true
-  url: https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  name: AdGuard Simplified Domain Names filter
-  id: 1
-- enabled: true
-  url: https://adaway.org/hosts.txt
-  name: AdAway
-  id: 2
-- enabled: true
-  url: https://hosts-file.net/ad_servers.txt
-  name: hpHosts - Ad and Tracking servers only
-  id: 3
-- enabled: true
-  url: https://www.malwaredomainlist.com/hostslist/hosts.txt
-  name: MalwareDomainList.com Hosts List
-  id: 4
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/2.txt
-  name: Base filter
-  id: 5
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/3.txt
-  name: Tracking Protection filter
-  id: 6
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/4.txt
-  name: Social media filter
-  id: 7
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/14.txt
-  name: Annoyances filter
-  id: 8
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/10.txt
-  name: Filter unblocking search ads and self-promotions
-  id: 9
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/5.txt
-  name: Experimental filter
-  id: 10
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/11.txt
-  name: Mobile ads filter
-  id: 11
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/12.txt
-  name: Safari filter
-  id: 12
-- enabled: true
-  url: https://filters.adtidy.org/extension/chromium/filters/15.txt
-  name: Simplified domain names filter
-  id: 13
-user_rules: []
-dhcp:
-  enabled: false
-  interface_name: "eth0"
-  gateway_ip: "10.10.10.10"
-  subnet_mask: "255.255.255.0"
-  range_start: "10.10.10.50"
-  range_end: "10.10.10.80"
-  lease_duration: 86400
-  icmp_timeout_msec: 1000
-clients: []
-log_file: ""
-verbose: false
-schema_version: 3' > $(pwd)/etc-adguardconf/AdGuardHome.yaml
+			cp $(pwd)/AdGuardHome.yaml $(pwd)/etc-adguardconf/
 			if [ -d $(pwd)/adguardwork ];
 			then
 				mkdir $(pwd)/etc-adguardwork/
 				cp -R $(pwd)/adguardwork/* $(pwd)/etc-adguardwork/
 			fi
-			docker run -d \
-				--name adguardhome \
-				--sysctl net.ipv6.conf.all.disable_ipv6=1 \
-				-p ${lanip}:5353:53/tcp -p ${lanip}:5353:53/udp \
-				-v "$(pwd)/etc-adguardwork/:/opt/adguardhome/work" \
-				-v "$(pwd)/etc-adguardconf/:/opt/adguardhome/conf" \
-				${1}
+			dockerstr+="--name adguardhome "
+			dockerstr+="-p ${lanip}:5353:53/tcp -p ${lanip}:5353:53/udp "
+			dockerstr+="-v $(pwd)/etc-adguardwork/:/opt/adguardhome/work/ "
+			dockerstr+=" -v $(pwd)/etc-adguardconf/:/opt/adguardhome/conf/ "
+		;;
+		"nginx:latest")
+			if [ ! -d $(pwd)/etc-html ];
+			then
+				mkdir $(pwd)/etc-html/
+				echo "<h1>Hello World</h1>" > $(pwd)/etc-html/index.html
+			fi
+			mkdir -p $(pwd)/etc-nginxconf/conf.d/
+			mkdir -p $(pwd)/etc-ssl/certs
+			cp nginx/nginx.conf nginx/mime.types nginx/fastcgi_params $(pwd)/etc-nginxconf/
+			cp nginx/conf.d/default.conf $(pwd)/etc-nginxconf/conf.d/
+			cp nginx.crt nginx.key $(pwd)/etc-ssl/
+			dockerstr+="--name nginx "
+			dockerstr+="-p ${lanip}:80:80/tcp -p ${lanip}:443:443/tcp "
+			dockerstr+="-v $(pwd)/etc-html/:/usr/share/nginx/html/ "
+			dockerstr+="-v $(pwd)/etc-nginxconf/:/etc/nginx/ "
+			dockerstr+="-v $(pwd)/etc-ssl/:/etc/ssl/ "
+			dockerstr+="--link php-fpm "
+		;;
+		"php:rc-fpm")
+			dockerstr+="--name php-fpm "
+			dockerstr+="-p ${lanip}:80:80/tcp -p ${lanip}:443:443/tcp "
+			dockerstr+="-v $(pwd)/etc-html/:/var/www/html/ "
 		;;
 	esac
+	dockerstr+="--sysctl net.ipv6.conf.all.disable_ipv6=1 "
+	docker run -d \
+		${dockerstr} \
+		${1}
 }
 
 stop() {
@@ -194,6 +118,9 @@ stop() {
 		;;
 		"adguard/adguardhome:arm64-edge")
 			rm -rf $(pwd)/etc-adguardconf $(pwd)/etc-adguardwork
+		;;
+		"nginx:latest")
+			rm -rf $(pwd)/etc-nginxconf $(pwd)/etc-ssl
 		;;
 	esac
 }
@@ -234,7 +161,7 @@ status() {
 				return 1
 			fi
 		;;
-		"adguard/adguardhome:arm64-edge")
+		"adguard/adguardhome:arm64-edge" | "nginx:latest")
 			if [ "$(docker inspect -f "{{.State.Status}}" ${container})" == "running" ] ; then
 				echo "Status [${1}]: $(docker inspect -f "{{.State.Status}}" ${container}), $(docker inspect -f "{{.State.Running}}" ${container})"
 				return 1
